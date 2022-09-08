a236e771f5353dc81d80a4d346b0a3fb14fafbf9
Yoma <yogolf2011@gmail.com>
2022-09-08 21:07:46 +0700

    Add turret & Add level scene

--- Assets/Scripts/Enemy/Turret.cs
+++ Assets/Scripts/Enemy/Turret.cs
@@ -1,33 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Turret : MonoBehaviour
 {
-    public enum AIState { Idle, Patrol, Attack, }
+    public enum AIState { Idle, Attack }
     private AIState currentState;
 
     [SerializeField] private float _hp;
 
     [Header("AI REFERENCES")]
     [SerializeField] private float _maxDistance;
-    [SerializeField] private Animator _anim;
+    [SerializeField] private Transform _headPos;
     private Transform _targetPos;
-    private NavMeshAgent _navMesh;
 
     [Header("GUN REFERENCES")]
     [SerializeField] private GameObject _bulletPrefabs;
     [SerializeField] private Transform _gunPos;
     [SerializeField] private int _ammo;
 
-    private CapsuleCollider _collider;
+    private BoxCollider _collider;
     bool isDie;
 
     void Start()
     {
-        _navMesh = GetComponent<NavMeshAgent>();
-        _collider = GetComponent<CapsuleCollider>();
+        _collider = GetComponent<BoxCollider>();
         _targetPos = GameObject.FindGameObjectWithTag("Player").transform;
         if (_targetPos == null) this.enabled = false;
 
@@ -45,7 +42,6 @@ public class Enemy : MonoBehaviour
         else
         {
             Idle();
-            Patrol();
         }
     }
 
@@ -57,9 +53,6 @@ public class Enemy : MonoBehaviour
             case AIState.Idle:
                 Idle();
                 break;
-            case AIState.Patrol:
-                Patrol();
-                break;
             case AIState.Attack:
                 StartCoroutine(Attack());
                 break;
@@ -67,59 +60,45 @@ public class Enemy : MonoBehaviour
         currentState = state;
     }
 
-    private void Idle() //CHECK PLAYER
+    private void Idle() //FIND PLAYER
     {
         if (currentState != AIState.Idle) return;
-        _navMesh.isStopped = true;
-        _anim.SetBool("isPatrol", false);
 
         if (Vector3.Magnitude((this.transform.position - _targetPos.transform.position)) <= _maxDistance)
         {
-            StateChange(AIState.Patrol);
-        }
-    }
-
-    private void Patrol() //CHASE PLAYER
-    {
-        if (currentState != AIState.Patrol) return;
-        _navMesh.isStopped = false;
-        _navMesh.destination = _targetPos.position;
-        _anim.SetBool("isPatrol", true);
-
-        if (_navMesh.remainingDistance <= _navMesh.stoppingDistance)
-        {
             StateChange(AIState.Attack);
-        }
-        if(_navMesh.remainingDistance >= _maxDistance)
-        {
-            StateChange(AIState.Idle);
         }
     }
 
     IEnumerator Attack() //ATTACK PLAYER
     {
         if (currentState != AIState.Attack) yield return null;
-        _navMesh.isStopped = true;
-        _anim.SetBool("isPatrol", false);
-        this.transform.LookAt(_targetPos.position);
+       
+        float t = 0;
+        while (t < 1.5f)
+        {
+            Vector3 direction = (_targetPos.position - _headPos.position).normalized;
+            Quaternion rotGoal = Quaternion.LookRotation(direction);
+            _headPos.rotation = Quaternion.Slerp(_headPos.rotation, rotGoal, 5f);
+            yield return new WaitForSeconds(0.01f);
+            t += 0.01f;
+        }
 
         for (int i = 0; i < _ammo; i++)
         {
-            yield return new WaitForSeconds(0.2f);
-            Instantiate(_bulletPrefabs, _gunPos.position, _gunPos.rotation);           
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(_bulletPrefabs, _gunPos.position, _gunPos.rotation);
         }
 
         yield return new WaitForSeconds(1f);
-        StateChange(AIState.Patrol);
+        StateChange(AIState.Idle);
     }
     #endregion
 
     IEnumerator Die()
     {
-        _anim.SetTrigger("Death");
         _collider.enabled = false;
-        _navMesh.isStopped = true;
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.5f);
         //Boom Effect;
         Destroy(this.gameObject);
     }
